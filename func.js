3e94f5f18b66926d4eb272907d87be8c3c276da2
JTen <therealzenzenzense@gmail.com>
2026-05-05 12:12:05 +0800

    Add files via upload

---
+++ func.js
@@ -0,0 +1,32 @@
+let hlCard = document.getElementById('hl_card');
+let aeCard = document.getElementById('ae_card');
+let rcCard = document.getElementById('rc_card');
+let ciCard = document.getElementById('ci_card');
+let saCard = document.getElementById('sa_card');
+let dtCard = document.getElementById('dt_card');
+let caCard = document.getElementById('ca_card');
+let programsCard = document.getElementById('programs_card');
+
+// History & Legacy Card
+hlCard.addEventListener('click', function(){
+    window.open('https://app.capabara.ai/dashboard/chat/69f892a6d50d535e7c7b1f58', '_blank'); /* This is a test link, replace with actual link when available */
+})
+
+// Academic Excellence Card
+
+// Research & Collaboration Card
+rcCard.addEventListener('click', function(){
+    window.open('https://app.capabara.ai/dashboard/chat?toolId=0cd832e2-c005-44c8-a898-2b2ce5fc868d', '_blank');
+})
+// Community Impact Card
+ciCard.addEventListener('click', function(){
+    window.open('https://app.capabara.ai/dashboard/chat?toolId=0250cd4e-2f71-48f7-87ad-a4090e878c08', '_blank');
+})
+// Student Achievements Card
+
+// Digital Transformation Card
+
+// Culture & Arts Card
+caCard.addEventListener('click', function(){
+    window.open('https://app.capabara.ai/dashboard/chat?toolId=51b48ba1-34c3-42f7-8936-45219f6a5d50', '_blank');
+})
